d73a8a3844a65e5c215efe90c6f8d62e40b781d2
TobiasPrieto <tobiasprieto10@gmail.com>
2023-09-08 19:08:23 -0300

    Correccion de script generador, falta la tarea con cron
    Signed-off-by: TobiasPrieto <tobiasprieto10@gmail.com>

--- estudiantes/prieto-tobias/tp/generador.sh
+++ estudiantes/prieto-tobias/tp/generador.sh
@@ -1,7 +1,17 @@
+
 #!/bin/bash
 
-base64 /dev/urandom | head -c 500 > file.txt
+VARIABLE=$((RANDOM%3))
+echo $VARIABLE
+#base64 /dev/urandom | head -c 500 > file.txt
+#
+#ffmpeg -f lavfi -i "sine=frequency=1000:duration=5" out.wav
 
-ffmpeg -f lavfi -i "sine=frequency=1000:duration=5" out.wav
-
-convert -size 100x100 xc: +noise Random noise.png
+#convert -size 100x100 xc: +noise Random noise.png
+if [[ $VARIABLE == 0 ]] ; then
+	base64 /dev/urandom | head -c 500 > file.txt
+elif [[ $VARIABLE == 1 ]] ; then
+	ffmpeg -f lavfi -i "sine=frequency=1000:duration=5" out.wav
+else
+	convert -size 100x100 xc: +noise Random noise.png
+fi
