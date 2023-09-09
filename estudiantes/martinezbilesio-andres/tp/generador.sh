0d1669bf8944a36ea84e3ee942737917736b7baa
Andres Martinez Bilesio <amartinezbilesio@gmail.com>
2023-09-08 21:10:03 -0300

    Agregado de archivo generador

---
+++ estudiantes/martinezbilesio-andres/tp/generador.sh
@@ -0,0 +1,25 @@
+
+# Programa generador de archivos capaz de generar distintos tipo de archivos
+
+opcion=$(echo $((RANDOM%3)))
+
+case $opcion in
+    0)
+        base64 /dev/urandom | head -c 500 > file.txt
+        hash=$(sha256sum file.txt | cut -d ' ' -f 1)
+        mv file.txt "$hash"
+    ;;
+    1)
+        ffmpeg -f lavfi -i "sine=frequency=1000:duration=5" out.wav
+        hash=$(sha256sum out.wav | cut -d ' ' -f 1)
+        mv out.wav "$hash"
+    ;;
+    2)
+        convert -size 100x100 xc: +noise Random noise.png
+        hash=$(sha256sum noise.png | cut -d ' ' -f 1)
+        mv noise.png "$hash"
+    ;;
+    *)
+        echo "Opción no válida: $opcion"
+    ;;
+esac
