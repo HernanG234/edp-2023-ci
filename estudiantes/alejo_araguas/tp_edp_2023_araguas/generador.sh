93c3dce60be9ba16f58b902c2809fc7a916a15a1
solange moreyra <solange.benitezok@gmail.com>
2023-09-15 19:43:04 -0300

    Modificacion script 'generador.sh'.
    Signed-off-by: solange moreyra <solange.benitezok@gmail.com>

--- estudiantes/alejo_araguas/tp_edp_2023_araguas/generador.sh
+++ estudiantes/alejo_araguas/tp_edp_2023_araguas/generador.sh
@@ -1,31 +1,18 @@
 #!/bin/bash
 
-echo ¿Que tipo de archivo deseea generar?
-echo 1- Archivo de texto.
-echo 2- Archivo de sonido.
-echo 3- Archivo de imágen.
-echo 0- Salir al menú.
-echo Escriba el número de su selección: ; read opc
-
-if [ $opc -eq 1 ]; then
-	base64 /dev/urandom | head -c 500 > file.txt
-	echo ¡Archivo de texto generado!
-	exit 1
-elif [ $opc -eq 2 ]; then
-	ffmpeg -f lavfi -i "sine=frequency=1000:duration=5" out.wav
-	echo ¡Archivo de sonido generado!
-	exit 1
-elif [ $opc -eq 3 ]; then
-	convert -size 100x100 xc: +noise Random noise.png
-	echo ¡Archivo de imágen generado!
-	exit 1
-elif [ $opc -eq 0 ]; then
-	echo Saliendo al menú...
-	exit 1
+var=$((RANDOM%3))
+if [ $var -eq 0 ]; then
+	base64 /dev/urandom | head -c 500 > file.txt  #texto
+elif [ $var -eq 1 ]; then
+	convert -size 100x100 xc: +noise random_noise.png  #imagenes
 else
-	echo Número incorrrecto.
-	exit 0
+	ffmpeg -f lavfi -i "anoisesrc=a=0.1:c=white:duration=5" out.wav  #sonidos
 fi
-convert -size 100x100 xc: +noise Random noise.png
 
-	base64 /dev/urandom | head -c 500 > file.txt
+bash=$(md5sum archivo|cut -d ' ' -fmd5sum archivo|cut -d ' ' -f1)
+
+mv $bash output/
+
+
+
+#docker run -d -p 21:21 -p 21000-21010:21000-21010 -e USERS="tuiaedpuser|tuiaedpuser" -v <ruta al outputs del generador>:/ftp/tuiaedpuser delfer/alpine-ftp-server
