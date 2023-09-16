bcacc74b21403350d226d90a0e95446de8fb986a
Andres Martinez Bilesio <amartinezbilesio@gmail.com>
2023-09-15 21:15:44 -0300

    Modificación generador y creación de carpeta output

--- estudiantes/martinezbilesio-andres/tp/generador.sh
+++ estudiantes/martinezbilesio-andres/tp/generador.sh
@@ -1,23 +1,25 @@
 
+
 # Programa generador de archivos capaz de generar distintos tipo de archivos
 
 opcion=$(echo $((RANDOM%3)))
+mkdir output
 
 case $opcion in
     0)
         base64 /dev/urandom | head -c 500 > file.txt
         hash=$(sha256sum file.txt | cut -d ' ' -f 1)
-        mv file.txt "$hash"
+        mv file.txt "output/$hash"
     ;;
     1)
         ffmpeg -f lavfi -i "sine=frequency=1000:duration=5" out.wav
         hash=$(sha256sum out.wav | cut -d ' ' -f 1)
-        mv out.wav "$hash"
+        mv out.wav "output/$hash"
     ;;
     2)
         convert -size 100x100 xc: +noise Random noise.png
         hash=$(sha256sum noise.png | cut -d ' ' -f 1)
-        mv noise.png "$hash"
+        mv noise.png "output/$hash"
     ;;
     *)
         echo "Opción no válida: $opcion"
