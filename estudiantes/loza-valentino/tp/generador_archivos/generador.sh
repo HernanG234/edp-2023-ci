0833572f53fad93e353724a530131e9ff13e3b2d
Athaly <tiniski028@gmail.com>
2023-09-19 12:07:04 -0300

    finished generador.sh
    Signed-off-by: Athaly <tiniski028@gmail.com>

--- estudiantes/loza-valentino/tp/generador_archivos/generador.sh
+++ estudiantes/loza-valentino/tp/generador_archivos/generador.sh
@@ -1,35 +1,24 @@
 #!/bin/bash
 
-generate_text() {
-	local filename="text_$(date +%Y%m%d%H%M%S)_$(uuidgen).txt"
-	base64 /dev/urandom | head -c 500 > "$filename"
-	echo "Texto aleatorio generado correctamente: $filename"
-}
+random_number=$((1 + RANDOM % 3))
+file_name="file_$(date '+%Y%m%d%H%M%S')."
+output="./output"
+mkdir -p "$output"
+case $random_number in
+    1)
+        base64 /dev/urandom | head -c 500 > "$file_name.txt"
+        ;;
+    2)
+        ffmpeg -f lavfi -i "anoisesrc=a=0.1:c=white:duration=5" "$file_name.wav"
+        ;;
+    3)
+        convert -size 100x100 xc: +noise Random "$file_name.png"
+        ;;
+    *)
+        ;;
+esac
 
-generate_sound() {
-	local filename="sound_$(date +%Y%m%d%H%M%S)_$(uuidgen).wav"
-	ffmpeg -f lavfi -i "sine=frequency=1000:duration=5" "$filename"
-	echo "Sonido aleatorio generado correctamente: $filename"
-}
+file_hash=$(sha256sum "$file_name"* | awk '{print $1}')
 
-generate_images() {
-	local filename="image_$(date +%Y%m%d%H%M%S)_$(uuidgen).png"
-	convert -size 100x100 xc: +noise Random "$filename"
-	echo "Imagen aleatoria generada correctamente: $filename"
-}
-
-output_dir="~/edp-2023-ci/estudiantes/loza-valentino/tp/generador_archivos/"
-
-mkdir -p "$output_dir"
-
-generate_text
-generate_sound
-generate_images
-
-for file in "$output_dir"/*; do
-    if [ -f "$file" ]; then
-        hash=$(sha256sum "$file" | awk '{print $1}')
-        mv "$file" "${file%.*}_$hash.${file##*.}"
-        echo "Archivo renombrado a: ${file%.*}_$hash.${file##*.}"
-    fi
-done
+mv "$file_name"* "$file_hash"
+mv "$file_hash" "$output/"
