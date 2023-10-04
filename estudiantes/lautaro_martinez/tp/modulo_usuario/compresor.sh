aabc5e2b47e2f693eb2562ba09208a72f0befdd7
lautaronmartinez <143416891+lautaronmartinez@users.noreply.github.com>
2023-10-04 16:15:26 -0300

    me equivoque en la ruta. La corregi

--- estudiantes/lautaro_martinez/tp/modulo_usuario/compresor.sh
+++ estudiantes/lautaro_martinez/tp/modulo_usuario/compresor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Ruta relativa al directorio del script
-base_dir=".."
+base_dir="."
 
 # Comprimir la carpeta outputs en un archivo outputs.tar
 tar -cvf "$base_dir/outputs.tar" -C "$base_dir" outputs
